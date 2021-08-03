7f7ab6be1b8ad90bbfbd75d67290ce76e65b0bb6
OnceRobert <robpurugganan@gmail.com>
2021-08-03 21:14:52 +0800

    [posmachine][purugro]feat: Added countItems

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,28 +1,40 @@
 package pos.machine;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+
+import static pos.machine.convertedItem.*;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
-        List<Object> Yes = convertBarcodes(barcodes);
-        return null;
+        List<Object> convertedBC = convertBarcodes(barcodes);
+        countItems(barcodes,convertedBC);
+        String receipt = "";
+        System.out.print(receipt);
+        return receipt;
     }
-
-    public List<Object> convertBarcodes(List<String> barcodes){
+    public List<Object> convertBarcodes(List<String> barcodes) {
         int Length = 0;
         ItemDataLoader IDL = new ItemDataLoader();
         List<ItemInfo> Items = IDL.loadAllItemInfos();
         List<Object> convertedBarcodes = new ArrayList<>();
-        for(ItemInfo Item : Items){
-            ItemInfo item1Info = new ItemInfo("ITEM000000", "Coca-Cola", 3);
-            Object Converted = {"Barcode" : Item.getBarcode(), "Name" : Item.getName(), "Price" : Item.getPrice(), "Quantity" : 0};
+        for (ItemInfo Item : Items) {
+            convertedItem Converted = new convertedItem(Item.getBarcode(), Item.getName(), Item.getPrice(), 0);
             convertedBarcodes.add(Converted);
         }
-
-
         return convertedBarcodes;
+    }
+    public void countItems(List<String> barcodes, List<Object> convertedItems) {
+        for (String BC : barcodes) {
+            for (Object CBarC : convertedItems) {
+                convertedItem Temp = (convertedItem) CBarC;
+                //String Temp = CBarC.getBarcode();
+                if (BC == Temp.getBarcode()) {
+                    int NewQ = Temp.getQuantity() + 1;
+                    Temp.setQuantity(NewQ);
+                }
+            }
+        }
     }
 
 
